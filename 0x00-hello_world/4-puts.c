c5f1f07beaf98e053d1ee81400b3d1378152ce45
Marvillas <agelv4real@gmail.com>
2023-03-16 20:25:04 -0700

    added c executable

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
+	/* The main fuction does not take any input, it prints out a statement */
 	puts("Programming is like building a multilingual puzzle");
-    return (0);
+	return (0);
 }
 
