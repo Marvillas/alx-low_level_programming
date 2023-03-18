bbab84a060e89b031c8c27fe92fbd43d5258936e
Marvillas <agelv4real@gmail.com>
2023-03-18 14:32:46 -0700

    positive anything is better than negative nothing

---
+++ 0x00-hello_world/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - The program print out either +ve or -ve
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		/* if condition is true then print +ve */
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
