d270b31f1417995c5ba29bb186714429f7fac331
Marvillas <agelv4real@gmail.com>
2023-03-19 20:05:28 -0700

    added the c code for 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - This program print the lower case of alphabet.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
