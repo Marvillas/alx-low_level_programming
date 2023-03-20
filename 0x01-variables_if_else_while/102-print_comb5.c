3a2b63ce7e785fca86664c12b12bac66a6a868d1
Marvis Aigbe <agelv4real@gmail.com>
2023-03-20 19:02:54 +0000

    added project 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main -  program that prints all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+    int one_digit = 0;
+    int sec_digit;
+
+    while (one_digit <= 98)
+    {
+        sec_digit = one_digit + 1;
+
+        while (sec_digit <= 99)
+        {
+            putchar(one_digit / 10 + '0');
+            putchar(one_digit % 10 + '0');
+            putchar(' ');
+            putchar(sec_digit / 10 + '0');
+            putchar(sec_digit % 10 + '0');
+
+            if (one_digit != 98 || sec_digit != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+            sec_digit++;
+        }
+    }
+    putchar('\n');
+
+    return (0);
+}
