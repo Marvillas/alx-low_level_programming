bc081cf5771d8ee01d04cd8980aafe6d3d9b5c6d
Marvillas <agelv4real@gmail.com>
2023-03-21 18:38:28 -0700

    added 0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes a character to std output
+ *
+ * @c: the character to write
+ *
+ * Return: Always 1 (success)
+ */
+int _putchar(char c);
+
+#endif /* MAIN_H */
