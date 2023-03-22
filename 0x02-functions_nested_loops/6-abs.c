30e97ef92e7a57321efae462b767eaf265b192b5
Marvillas <agelv4real@gmail.com>
2023-03-22 00:09:46 -0700

    added the function file for checking absolute value of any number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - checks for lowercase character
+ * @n: the character to check
+ *
+ * Return: Always 0 (success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
