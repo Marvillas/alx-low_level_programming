4e7301b1a9bb5e54b1cb24aee4b3636aff3cb6e4
Marvillas <agelv4real@gmail.com>
2023-03-21 19:39:44 -0700

    added 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c = 'a';
+
+	while (i < 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
