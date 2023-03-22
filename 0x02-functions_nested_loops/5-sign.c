a206b5c2f6a4d259d804e16beeba094002af1e98
Marvillas <agelv4real@gmail.com>
2023-03-21 22:44:23 -0700

    Added file 5-sign .c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/**
+ * print_sign - checks for lowercase character
+ * @n: the character to check
+ *
+ * Return: 1 if c is lowercase, 0 success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
