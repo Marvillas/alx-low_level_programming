960d41c4226f23c8c9b19c56ed8f3e063eb28ccd
Marvillas <agelv4real@gmail.com>
2023-03-29 13:19:12 -0700

    added some of the project files

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenate two strings, using at most n bytes from src.
+ * _strcat - Concatenate two strings, using at most n bytes from src.
  *
  * @dest: The destination string
  * @src: The source string.
  * @n: The maximum number of bytes to use from the source string.
  * Return: A pointer to the resulting null-terminated string dest.
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	return (strcat(dest, src));
 }
