9697975b3276dba722f53b9e4f32be42d7a00d13
Marvillas <agelv4real@gmail.com>
2023-03-29 13:20:43 -0700

    added some of the project files

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,6 @@
  *
  * @dest: The destination string
  * @src: The source string.
- * @n: The maximum number of bytes to use from the source string.
  * Return: A pointer to the resulting null-terminated string dest.
  */
 char *_strcat(char *dest, char *src)
