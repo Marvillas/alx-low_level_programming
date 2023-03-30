4774ca327fd680fe4745a1b20ca9468a8899092e
Marvillas <agelv4real@gmail.com>
2023-03-30 14:31:37 -0700

    added some of the project files

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * infinite_add - Adds two numbers represented as strings
  * @n1: The first number
@@ -11,34 +10,37 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1), len2 = strlen(n2), carry = 0, i = 0, j = 0, k;
+	int i, j, k, l, m, n;
 
-	if (len1 + len2 + 1 > size_r)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
-
-	for (i = 0; i < len1 || i < len2 || carry; i++)
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		if (i < len1)
-			carry += n1[len1 - i - 1] - '0';
-		if (i < len2)
-			carry += n2[len2 - i - 1] - '0';
-
-		r[i] = carry % 10 + '0';
-		carry /= 10;
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
-
-	if (i == 0)
-		r[i++] = '0';
-
-	r[i] = '\0';
-
-	for (j = 0, k = i - 1; j < k; j++, k--)
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
 	{
-		char tmp = r[j];
-
-		r[j] = r[k];
-		r[k] = tmp;
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
 	}
-
 	return (r);
 }
