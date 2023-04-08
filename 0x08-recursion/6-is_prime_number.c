d7e6f234d384593b7ab6453f621d30712e5b4b81
Marvillas <agelv4real@gmail.com>
2023-04-08 12:34:37 -0700

    modification of some files

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,15 +9,15 @@ int is_prime_number(int n)
 {
 	int i;
 
-	if (n < 2)
-		return (0);
-	i = 2;
-	while (i * i <= n)
+	if (n <= 1)
 	{
-		if (n % i == 0)
-			return (0);
-		i++;
+		return (0);
 	}
 
-	return (1);
+	i = 2;
+	if (n % i == 0)
+	{
+		return (1);
+	}
+
 }
