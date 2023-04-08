f44d0aefb5fcf97fea2c9b0dfa84f5d08c64a4c6
Marvillas <agelv4real@gmail.com>
2023-04-08 13:12:32 -0700

    modified 6 is prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,17 +7,25 @@
  */
 int is_prime_number(int n)
 {
-	int i;
+	return (is_prime(n, 1));
+}
 
+/**
+ * is_prime - checks prime number
+ * @n: integer argument number
+ * @i: int argument
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime(int n, int i)
+{
 	if (n <= 1)
-	{
 		return (0);
-	}
-
-	i = 2;
-	if (n % i == 0)
-	{
+	if (n <= 3)
 		return (1);
-	}
-
+	if (n % i == 0 && i > 1)
+		return (0);
+	if ((n / i) < i)
+		return (1);
+	return (is_prime(n, i + 1));
 }
